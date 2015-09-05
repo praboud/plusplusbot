e93031ebef5fcd2a8543c67ab2187e6927295e3d
Peter Raboud <praboud@gmail.com>
2015-09-05 14:46:13 -0700

    Switch to persistence with sqlite3

--- bot.py
+++ bot.py
@@ -2,21 +2,56 @@
 
 import irc.bot
 import irc.strings
-from irc.client import ip_numstr_to_quad
 import re
+import sqlite3
+import argparse
+import sys
 
 
 class PlusPlusBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel, nickname, server, port=6667):
+    def __init__(self, dbconn, channel, nickname, server, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname,
                                             nickname)
+        self.dbconn = dbconn
         self.channel = channel
         self.record = {}
+
+        self.setup_db()
+
+    def setup_db(self):
+        self.dbconn.execute('''
+                CREATE TABLE IF NOT EXISTS karma
+                (key TEXT PRIMARY KEY, score INTEGER);
+            ''')
+
+        self.dbconn.isolation_level = 'EXCLUSIVE'  # prevent races on updates
+
+    def increment_karma(self, key, increment=1):
+        c = self.dbconn.cursor()
+
+        c.execute('SELECT score FROM karma WHERE key=?', (key,))
+
+        row = c.fetchone()
+
+        if row is None:
+            score = 0
+        else:
+            score = row[0]
+
+        score += increment
+
+        c.execute('INSERT OR REPLACE INTO karma (key, score) VALUES (?, ?)',
+                  (key, score))
+
+        self.dbconn.commit()
+
+        return score
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
 
     def on_welcome(self, c, e):
+        print("welcome")
         c.join(self.channel)
 
     def on_nosuchchannel(self, c, e):
@@ -24,8 +59,7 @@ class PlusPlusBot(irc.bot.SingleServerIRCBot):
 
     def handle_increment(self, match):
         target = match.group(1)
-        val = self.record.get(target, 0) + 1
-        self.record[target] = val
+        val = self.increment_karma(target)
 
         msg = "{}++, now at {}".format(target, val)
         self.connection.privmsg(self.channel, msg)
@@ -46,12 +80,15 @@ class PlusPlusBot(irc.bot.SingleServerIRCBot):
 
 
 def main():
-    import sys
-    if len(sys.argv) != 4:
-        print("Usage: testbot <server[:port]> <#channel> <nickname>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='IRC karma bot')
+    parser.add_argument('dbname', help='Filename for sqlite db')
+    parser.add_argument('server', metavar='server[:port]')
+    parser.add_argument('channel')
+    parser.add_argument('nickname')
 
-    s = sys.argv[1].split(":", 1)
+    args = parser.parse_args()
+
+    s = args.server.split(":", 1)
     server = s[0]
     if len(s) == 2:
         try:
@@ -61,10 +98,10 @@ def main():
             sys.exit(1)
     else:
         port = 6667
-    channel = sys.argv[2]
-    nickname = sys.argv[3]
 
-    bot = PlusPlusBot(channel, nickname, server, port)
+    dbconn = sqlite3.connect(args.dbname)
+
+    bot = PlusPlusBot(dbconn, args.channel, args.nickname, server, port)
     bot.start()
 
 if __name__ == "__main__":
